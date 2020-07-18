471582f64f82c1419573847cf1e792ff39268c0e
Harsha Vardhan <Harsha.Vardhan@agoda.com>
2020-07-19 03:17:30 +0700

    add header page with logo, shop and contact links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const Hats = () => {
   return (
@@ -14,6 +15,7 @@ const Hats = () => {
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop/hats" component={Hats} />
