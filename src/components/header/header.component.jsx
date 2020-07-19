13c947bfafd085f18ca00e466dc12e6ab8cf7422
Harsha Vardhan <Harsha.Vardhan@agoda.com>
2020-07-19 18:01:33 +0700

    add sigin and signout page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => {
+const Header = ({currentUser}) => {
     return (
         <div className="header">
             <Link className="logo-container" to="/">
                 <Logo className="logo"/>
             </Link>
             <div className="options">
-                <Link className="option" to="/Shop"> Shop </Link>
-                <Link className="option" to="/Shop"> Contact </Link>
+                <Link className="option" to="/shop"> Shop </Link>
+                <Link className="option" to="/shop"> Contact </Link>
+                {currentUser ? 
+                <div className="option" onClick={() => auth.signOut()}> Sign out </div>
+                : 
+                <Link className="option" to="/signin"> Sign In </Link>
+                }
             </div>
         </div>
     )
